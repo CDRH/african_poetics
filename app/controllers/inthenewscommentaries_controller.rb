58a87ce0296afc2110a0cef8f34af87f71ea0a24
William Dewey <wkdewey@gmail.com>
2022-09-28 15:21:18 -0500

    remove show action which can be handled by sections

--- app/controllers/inthenewscommentaries_controller.rb
+++ app/controllers/inthenewscommentaries_controller.rb
@@ -16,21 +16,4 @@ class InthenewscommentariesController < ItemsController
     @skip_fields = []
   end
 
-  def show
-    id = params["id"]
-    @res = @items_api.get_item_by_id(id)
-    @res = @res.first
-    if @res
-      url = @res["uri_html"]
-      @html = Net::HTTP.get(URI.parse(url)) if url
-      @title = item_title
-
-      render_overridable("inthenewscommentaries", "show")
-    else
-      @title = t "item.no_item", id: id,
-        default: "No item with identifier #{id} found!"
-      render_overridable("items", "show_not_found", status: 404)
-    end
-  end
-
 end
