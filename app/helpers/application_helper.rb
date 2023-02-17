0b1d5d4b023f2652ce8f3138f5159c367145e2a3
William Dewey <wkdewey@gmail.com>
2023-02-17 16:15:47 -0600

    change regex to only match first occurrence of brackets and parentheses
    This will fix broken links, but means that not everything will be parsed.
    If something drops out because of this change, it indicates a problem with
    data in airtable, or else with parsing into the API

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   include Orchid::ApplicationHelper
 
   def parse_md_brackets(query)
-    /\[(.*)\]/.match(query)[1] if /\[(.*)\]/.match(query)
+    /\[(.*?)\]/.match(query)[1] if /\[(.*?)\]/.match(query)
   end
 
   def parse_md_parentheses(query)
-    /\]\((.*)\)/.match(query)[1] if /\]\((.*)\)/.match(query)
+    /\]\((.*?)\)/.match(query)[1] if /\]\((.*?)\)/.match(query)
   end
 end
