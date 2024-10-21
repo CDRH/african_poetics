83abdd137c125e16e596582247c5eb9ecf97b1bf
Erin Chambers <echambers4@unl.edu>
2024-10-21 11:39:26 -0500

    Convert Critical Bibliographies to section and add new views

---
+++ app/controllers/bibliographies_controller.rb
@@ -0,0 +1,27 @@
+class BibliographiesController < ApplicationController
+
+  def bibliographies_kofi_awoonor
+    @title = "The Life and Works of Kofi Awoonor"
+  end
+
+  def bibliographies_tanella_boni
+    @title = "The Life and Works of Tanella Boni"
+  end
+
+  def bibliographies_keorapetse_kgositsile
+    @title = "The Life and Works of Keorapetse Kgositsile"
+  end
+
+  def bibliographies_antjie_krog
+    @title = "The Life and Works of Antjie Krog"
+  end
+
+  def bibliographies_gabriel_okara
+    @title = "The Life and Works of Gabriel Okara"
+  end
+
+  def bibliographies_wole_soyinka
+    @title = "The Life and Works of Wole Soyinka"
+  end
+
+end
